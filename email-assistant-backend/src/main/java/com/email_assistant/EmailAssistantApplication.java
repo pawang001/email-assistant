9aaaa76b05a14717526e2248ead9df9edaf5ecfa
Pawan Kumar Gupta <pawankumargupta365@gmail.com>
2026-02-15 11:42:25 +0530

    Add DockerFile

--- email-assistant-backend/src/main/java/com/email_assistant/EmailAssistantApplication.java
+++ email-assistant-backend/src/main/java/com/email_assistant/EmailAssistantApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmailAssistantApplication {	
+public class EmailAssistantApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailAssistantApplication.class, args);
